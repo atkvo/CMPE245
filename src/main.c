9950e0a6b592f050e8dd81c3fa241d7ab1cb49b8
Andrew Vo <voandrew.khoa@gmail.com>
2016-10-09 15:32:06 -0700

    added commands to start/stop/setrate of timer

--- src/main.c
+++ src/main.c
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
+#include <string.h>
 #include "lpc17xx_libcfg.h"
 #include "uart_controls.h"
 #include "engine.h"
@@ -18,24 +19,10 @@ s_buff URX_BUF;
 
 const char GREET[] = "**** Welcome ****\n";
 const char GREET_WAIT_CMD[] = "Please enter your command and press enter: \n\r\t";
-void UART3_IRQHandler(void) {
-    if ((LPC_UART3->IIR & U_IIR_RDA_INT_ID) == U_IIR_RDA_INT_ID) {
-        if(URX_BUF.bits >= (BUF_SIZE - 1)) {
-            clearBuffer(&URX_BUF);
-            uprintf("Input buffer overrun.\r\n");
-        }
-        else {
-            char ch = uart_rx();
-            pushToBuffer(&URX_BUF, ch);
-            if(ch == '\n') {
-                uprintf("\tACK: %s", URX_BUF.stream);
-                uprintf(GREET_WAIT_CMD);
-                clearBuffer(&URX_BUF);
-                // command is entered
-            }
-        }
-    }
-}
+const char CMD_TIMER_START[] = "TIMERSTART";
+const char CMD_TIMER_STOP[] = "TIMERSTOP";
+const char CMD_TIMER_SET[] = "TIMERSET";
+const int CMD_TIMER_PARAM_INDEX = 8;
 
 #define RITINT   0
 #define RITENCLR 1
@@ -70,6 +57,45 @@ void RIT_IRQHandler(void) {
     LPC_RIT->RICTRL |= (1 << RITINT);
 }
 
+void UART3_IRQHandler(void) {
+    if ((LPC_UART3->IIR & U_IIR_RDA_INT_ID) == U_IIR_RDA_INT_ID) {
+        if(URX_BUF.bits >= (BUF_SIZE - 1)) {
+            clearBuffer(&URX_BUF);
+            uprintf("Input buffer overrun.\r\n");
+        }
+        else {
+            char ch = uart_rx();
+            if(ch == '\n') {
+                uprintf("\tACK: %s", URX_BUF.stream);
+                if (strcmp(CMD_TIMER_START, URX_BUF.stream) == 0) {
+                    ritStart();
+                }
+                else if (strcmp(CMD_TIMER_STOP, URX_BUF.stream) == 0) {
+                    ritStop();
+                }
+                else if (strstr(URX_BUF.stream, CMD_TIMER_SET) != NULL) {
+                    char * tmp;
+                    int newrate = strtol(&URX_BUF.stream[CMD_TIMER_PARAM_INDEX], &tmp, 10);
+                    if(newrate > 0) {
+                        uprintf("\tSetting rate to: %i Hz\n", newrate);
+                        ritStop();
+                        ritSetHz(newrate);
+                        ritStart();
+                    }
+                    else { 
+                        uprintf("\tInvalid rate: %i Hz\n", newrate);
+                    }
+                }
+                uprintf(GREET_WAIT_CMD);
+                clearBuffer(&URX_BUF);
+                // command is entered
+            }
+            else if (ch != '\r') {
+                pushToBuffer(&URX_BUF, ch);
+            }
+        }
+    }
+}
 int main(void)
 {
     SystemInit();
