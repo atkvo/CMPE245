9120fbca12547513c355089fd09305a38a4601b4
Andrew Vo <voandrew.khoa@gmail.com>
2016-10-09 12:58:21 -0700

    use the buffer methods from engine.h

--- src/main.c
+++ src/main.c
@@ -7,52 +7,26 @@
 #include <stdarg.h>
 #include "lpc17xx_libcfg.h"
 #include "uart_controls.h"
+#include "engine.h"
 
 #define MAX_PRINT_LENGTH 1024
 #define CHECK_BIT(var, pos) ((var) & (1<<(pos)))
 
-#define URX_BUF_LEN             1024
-// char URX_BUF[URX_BUF];
-typedef struct
-{
-    char stream[URX_BUF_LEN];
-    int i;
-} RX_BUFFER;
+//#define URX_BUF_LEN             1024
 
-RX_BUFFER URX_BUF;
+s_buff URX_BUF;
 
 const char GREET[] = "**** Welcome ****\n";
 const char GREET_WAIT_CMD[] = "Please enter your command and press enter: \n\r\t";
-
-
-void initBuffer(RX_BUFFER *buf) {
-    buf->i = 0;
-}
-
-void clearBuffer(RX_BUFFER *buf) {
-    buf->i = 0;
-    for(int i = 0; i < URX_BUF_LEN; i++) {
-        buf->stream[i] = 0;
-    }
-}
-
-void addToBuffer(RX_BUFFER *buf, char c) {
-    if(buf->i >= URX_BUF_LEN - 1) {
-        clearBuffer(buf);
-    } 
-    buf->stream[buf->i] = c;
-    buf->i++;
-}
-
 void UART3_IRQHandler(void) {
     if ((LPC_UART3->IIR & U_IIR_RDA_INT_ID) == U_IIR_RDA_INT_ID) {
-        if(URX_BUF.i >= (URX_BUF_LEN - 1)) {
+        if(URX_BUF.bits >= (BUF_SIZE - 1)) {
             clearBuffer(&URX_BUF);
             uprintf("Input buffer overrun.\r\n");
         }
         else {
             char ch = uart_rx();
-            addToBuffer(&URX_BUF, ch);
+            pushToBuffer(&URX_BUF, ch);
             if(ch == '\n') {
                 uprintf("\tACK: %s", URX_BUF.stream);
                 uprintf(GREET_WAIT_CMD);
